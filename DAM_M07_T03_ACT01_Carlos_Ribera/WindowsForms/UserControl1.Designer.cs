314ed94d63276d556422357b9ff8d0b57e1f29ab
Carlos Ribera Donet <carlosriberadonet@gmail.com>
2025-03-22 14:24:56 +0100

    Proyecto finalizado

--- DAM_M07_T03_ACT01_Carlos_Ribera/WindowsForms/UserControl1.Designer.cs
+++ DAM_M07_T03_ACT01_Carlos_Ribera/WindowsForms/UserControl1.Designer.cs
@@ -32,12 +32,15 @@
             this.buttonDel = new System.Windows.Forms.Button();
             this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
             this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.añadirX = new System.Windows.Forms.Button();
+            this.eliminarX = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(257, 297);
+            this.buttonAdd.Location = new System.Drawing.Point(306, 70);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(75, 23);
             this.buttonAdd.TabIndex = 0;
@@ -47,7 +50,7 @@
             // 
             // buttonDel
             // 
-            this.buttonDel.Location = new System.Drawing.Point(425, 297);
+            this.buttonDel.Location = new System.Drawing.Point(388, 70);
             this.buttonDel.Name = "buttonDel";
             this.buttonDel.Size = new System.Drawing.Size(75, 23);
             this.buttonDel.TabIndex = 1;
@@ -65,10 +68,40 @@
             this.pictureBox.TabIndex = 2;
             this.pictureBox.TabStop = false;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(345, 350);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // añadirX
+            // 
+            this.añadirX.Location = new System.Drawing.Point(306, 376);
+            this.añadirX.Name = "añadirX";
+            this.añadirX.Size = new System.Drawing.Size(75, 23);
+            this.añadirX.TabIndex = 7;
+            this.añadirX.Text = "Añadir";
+            this.añadirX.UseVisualStyleBackColor = true;
+            this.añadirX.Click += new System.EventHandler(this.añadirX_Click);
+            // 
+            // eliminarX
+            // 
+            this.eliminarX.Location = new System.Drawing.Point(387, 376);
+            this.eliminarX.Name = "eliminarX";
+            this.eliminarX.Size = new System.Drawing.Size(75, 23);
+            this.eliminarX.TabIndex = 8;
+            this.eliminarX.Text = "Eliminar";
+            this.eliminarX.UseVisualStyleBackColor = true;
+            this.eliminarX.Click += new System.EventHandler(this.eliminarX_Click);
+            // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.eliminarX);
+            this.Controls.Add(this.añadirX);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.buttonDel);
             this.Controls.Add(this.buttonAdd);
@@ -76,6 +109,7 @@
             this.Size = new System.Drawing.Size(800, 450);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +119,8 @@
         private System.Windows.Forms.Button buttonDel;
         private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
         private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button añadirX;
+        private System.Windows.Forms.Button eliminarX;
     }
 }
--- DAM_M07_T03_ACT01_Carlos_Ribera/WindowsForms/UserControl1.cs
+++ DAM_M07_T03_ACT01_Carlos_Ribera/WindowsForms/UserControl1.cs
@@ -22,15 +22,33 @@ namespace WindowsForms
         public UserControl1()
         {
             InitializeComponent();
-            pictureBox.Paint += PictureBoxPaint; // En Java utilizaba listeners, aqui he investigado y he visto que se hace mediante +=
+            pictureBox.Paint += PictureBoxPaint;
         }
 
         // Método para pintar el circulo de notificaciones
         private void PictureBoxPaint(object sender, PaintEventArgs e)
-        {           
+        {
+            string texto = "";
+
             Graphics g = e.Graphics; // Obtengo el Objeto Graphics para pintar
             int diametro = (int)(Math.Min(pictureBox.Width, pictureBox.Height) * 0.30); // 30% del tamaño del PictureBox
-
+              
+            // Límito por debajo
+            if (numeroNotificaciones <= 0)
+            {
+                numeroNotificaciones = 0;
+            }
+            // Límito por arriba
+            else if (numeroNotificaciones > 99)
+            {
+                String maxNotificaciones = "+99";
+                texto = maxNotificaciones;
+            }
+            else
+            {
+                texto = numeroNotificaciones.ToString();
+            }
+            
             if (numeroNotificaciones > 0)
             {
                 // Posicionamos el círculo en la esquina superior izquierda de PictureBox
@@ -44,7 +62,7 @@ namespace WindowsForms
                 }
 
                 // Dibujar el número dentro del círculo
-                string texto = numeroNotificaciones.ToString();
+                
                 using (Font fuente = new Font(Font.FontFamily, diametro / 2, FontStyle.Bold))
                 using (Brush brushTexto = new SolidBrush(Color.White))
                 {
@@ -53,12 +71,7 @@ namespace WindowsForms
                     float textoY = posY + (diametro - tamañoTexto.Height) / 2;
                     g.DrawString(texto, fuente, brushTexto, textoX, textoY);
                 }
-            }
-            if (numeroNotificaciones <= 0)
-            {
-                numeroNotificaciones = 0;
-            }
-                
+            }     
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
@@ -72,6 +85,18 @@ namespace WindowsForms
         {
             numeroNotificaciones--;
             Debug.WriteLine($"Número de notificaciones: {numeroNotificaciones}"); // Mensaje de depuración
+            pictureBox.Invalidate();
+        }
+
+        private void añadirX_Click(object sender, EventArgs e)
+        {
+            numeroNotificaciones += int.Parse(textBox1.Text);
+            pictureBox.Invalidate();
+        }
+
+        private void eliminarX_Click(object sender, EventArgs e)
+        {
+           numeroNotificaciones -= int.Parse(textBox1.Text);
             pictureBox.Invalidate();
         }
     }
